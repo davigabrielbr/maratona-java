007f31a01c548095a9bb26750b4e5461b28b5920
Davi Gabriel <davi_gabriel99@hotmail.com>
2025-12-04 13:50:30 -0300

    Praticando

---
+++ EstruturasCondicionais.java
@@ -0,0 +1,5 @@
+package maratonajava.com.davi.introducao;
+
+public class EstruturasCondicionais {
+
+}
--- Operadores.java
+++ Operadores.java
@@ -40,5 +40,23 @@ public class Operadores {
 
         boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
         System.out.println("isPlaystationCincoCompravel " + isPlaystationCincoCompravel);
+
+        // = += -= *= /= %=
+        double bonus = 1800;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.println(bonus);
+
+        // Incrementar ++ --
+        int contador = 0;
+        contador += contador; // contador = contador + 1;
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        System.out.println(contador);
     }
 }
