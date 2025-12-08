cbd175e7a84b23dec19965a13789a8aa9148a37e
Davi Gabriel <davi_gabriel99@hotmail.com>
2025-12-08 10:58:07 -0300

    Praticando break e continue.

---
+++ EstruturasDeRepeticaoBreak.java
@@ -0,0 +1,16 @@
+package maratonajava.com.davi.introducao;
+
+public class EstruturasDeRepeticaoBreak {
+    // Imprima os primeiros 25 números de um dado valor. Por exemplo, 50.
+    public static void main(String[] args) {
+        int valorMaximo = 50;
+
+        for (int i = 0; i <= valorMaximo; i++) {
+            if (i > 25) {
+                break;
+            }
+
+            System.out.println(i);
+        }
+    }
+}
---
+++ EstruturasDeRepeticaoBreakExercicio.java
@@ -0,0 +1,19 @@
+package maratonajava.com.davi.introducao;
+
+public class EstruturasDeRepeticaoBreakExercicio {
+    // Dado o valor de um carro, descubra em quantas vezes ele pode ser parcelado
+    // Condição valorParcela >= 1000
+    public static void main(String[] args) {
+        double valorTotal = 30000;
+
+        for (int parcela = 1; parcela < valorTotal; parcela++) {
+            double valorParcela = valorTotal / parcela;
+
+            if (valorParcela < 1000) {
+                break;
+            }
+
+            System.out.println("Parcela " + parcela + " R$ " + valorParcela);
+        }
+    }
+}
