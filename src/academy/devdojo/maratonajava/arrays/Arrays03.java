454dc19d50ba4b21889fc1d0e033ac57e63a6cc3
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-05-13 14:36:28 -0300

    Aula 36 - Arrays Multidimensionais pt 01

--- src/academy/devdojo/maratonajava/arrays/Arrays03.java
+++ src/academy/devdojo/maratonajava/arrays/Arrays03.java
@@ -6,9 +6,9 @@ public class Arrays03 {
         int[] numeros2 = {1, 2, 3, 4, 5};
         int[] numeros3 = new int[]{1, 2, 3, 4, 5};
 
-        for (int i = 0; i < numeros3.length; i++) {
-            System.out.println(numeros3[i]);
-        }
+//        for (int i = 0; i < numeros3.length; i++) {
+//            System.out.println(numeros3[i]);
+//        }
 
         for (int array : numeros3) {
             System.out.println(array);
