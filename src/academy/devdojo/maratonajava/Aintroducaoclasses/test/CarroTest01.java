a69d8866fb2bab0df17603ea1c879d648b6e67a2
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-06-18 15:55:33 -0300

    43 - Orientação Objetos - Referência de objetos

--- src/academy/devdojo/maratonajava/Aintroducaoclasses/test/CarroTest01.java
+++ src/academy/devdojo/maratonajava/Aintroducaoclasses/test/CarroTest01.java
@@ -15,6 +15,8 @@ public class CarroTest01 {
         carro2.modelo = "Q3";
         carro2.ano = 2026;
 
+        carro1 = carro2;
+
         System.out.println("Carro 1");
         System.out.println("Nome: " + carro1.nome);
         System.out.println("Modelo: " + carro1.modelo);
