b721896b806874e34a6f9a61ebd0afa1f8231284
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-07-07 14:17:33 -0300

    45 - Orientação Objetos - Métodos pt 02 - Parâmetros

---
+++ src/academy/devdojo/maratonajava/Bintroducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.Bintroducaometodos.model.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.multiplicaDoisNumeros(10, 20);
+    }
+}
