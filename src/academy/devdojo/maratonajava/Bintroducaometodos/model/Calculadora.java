cc9f266056e30f01c1d6796a6549cab3b1c37959
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-07-07 13:59:14 -0300

    44 - Orientação Objetos - Métodos pt 01

---
+++ src/academy/devdojo/maratonajava/Bintroducaometodos/model/Calculadora.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.Bintroducaometodos.model;
+
+public class Calculadora {
+    public void somaDoisNumeros() {
+        System.out.println(10 + 10);
+    }
+
+    public void subtraiDoisNumeros() {
+        System.out.println(21 - 2);
+    }
+}
---
+++ src/academy/devdojo/maratonajava/Bintroducaometodos/test/CalculadoraTest01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.Bintroducaometodos.model.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.somaDoisNumeros();
+        calculadora.subtraiDoisNumeros();
+    }
+}
