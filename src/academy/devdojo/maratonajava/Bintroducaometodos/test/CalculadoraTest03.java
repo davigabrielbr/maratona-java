97a9d45921e784630407f1b62fa2f6a52d2e159c
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-07-09 13:45:09 -0300

    46 - Orientação Objetos - Métodos pt 03 - Retorno pt 01

---
+++ src/academy/devdojo/maratonajava/Bintroducaometodos/test/CalculadoraTest03.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.Bintroducaometodos.model.Calculadora;
+
+public class CalculadoraTest03 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        double resultado = calculadora.divideDoisNumeros(20, 2);
+        System.out.println(resultado);
+
+        System.out.println(calculadora.divideDoisNumeros(20, 2));
+    }
+}
