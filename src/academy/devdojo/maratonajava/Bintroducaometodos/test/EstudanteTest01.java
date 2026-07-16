7fe5eef15bcd035893155c59118ff69886c31d41
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-07-16 19:44:49 -0300

    50 - Orientação Objetos - Métodos pt 07 - Parâmetros tipo referência pt 02

--- src/academy/devdojo/maratonajava/Bintroducaometodos/test/EstudanteTest01.java
+++ src/academy/devdojo/maratonajava/Bintroducaometodos/test/EstudanteTest01.java
@@ -19,5 +19,10 @@ public class EstudanteTest01 {
 
         impressora.imprime(estudante01);
         impressora.imprime(estudante02);
+
+        System.out.println("##################");
+
+        impressora.imprime(estudante01);
+        impressora.imprime(estudante02);
     }
 }
