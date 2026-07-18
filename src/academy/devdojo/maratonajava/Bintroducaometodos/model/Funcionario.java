a9abcbf5169d8f98d59850346c4bdbf2579c8045
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-07-18 17:47:20 -0300

    56 - Orientação Objetos - Modificador de acesso private, get e set pt 03

--- src/academy/devdojo/maratonajava/Bintroducaometodos/model/Funcionario.java
+++ src/academy/devdojo/maratonajava/Bintroducaometodos/model/Funcionario.java
@@ -1,9 +1,10 @@
 package academy.devdojo.maratonajava.Bintroducaometodos.model;
 
 public class Funcionario {
-    public String nome;
-    public int idade;
-    public double[] salarios;
+    private String nome;
+    private int idade;
+    private double[] salarios;
+    private double media;
 
     public void imprime() {
         System.out.println(this.nome);
@@ -25,8 +26,6 @@ public class Funcionario {
             return;
         }
 
-        double media = 0;
-
         for (double salario : salarios) {
             media += salario;
         }
@@ -34,5 +33,33 @@ public class Funcionario {
         media /= salarios.length;
 
         System.out.printf("\nMédia: %.2f", media);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public double getMedia() {
+        return media;
     }
 }
