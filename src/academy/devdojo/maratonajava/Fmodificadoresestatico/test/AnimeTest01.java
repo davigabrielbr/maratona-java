aaded221397dddae240b6ceba8c2f88bc1a21098
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-07-24 17:36:18 -0300

    63 - Orientação Objetos - Bloco de inicialização estático

---
+++ src/academy/devdojo/maratonajava/Fmodificadoresestatico/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.Fmodificadoresestatico.test;
+
+import academy.devdojo.maratonajava.Fmodificadoresestatico.model.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime1 = new Anime();
+        Anime anime2 = new Anime();
+        Anime anime3 = new Anime();
+    }
+}
