3e8eb147112b7c902804829bfc04801a87c6942a
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-07-29 19:18:02 -0300

    71 - Orientação Objetos - Herança pt 01

---
+++ src/academy/devdojo/maratonajava/Hheranca/test/HerancaTest01.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.Hheranca.test;
+
+import academy.devdojo.maratonajava.Hheranca.model.Endereco;
+import academy.devdojo.maratonajava.Hheranca.model.Funcionario;
+import academy.devdojo.maratonajava.Hheranca.model.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("012345-209");
+
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Toyohisa Shinazu");
+        pessoa.setCpf("111111111");
+        pessoa.setEndereco(endereco);
+
+        pessoa.imprime();
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Oda Nobunaga");
+        funcionario.setCpf("22222");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(20000);
+
+        System.out.println("---------------");
+        funcionario.imprime();
+    }
+}
