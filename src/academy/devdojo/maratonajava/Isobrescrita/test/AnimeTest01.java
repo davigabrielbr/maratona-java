24b92475e59a3a1a3759033b4f923bce484a0ea8
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-07-29 20:04:30 -0300

    76 - Orientação Objetos - Sobrescrita do método toString

---
+++ src/academy/devdojo/maratonajava/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.Isobrescrita.test;
+
+import academy.devdojo.maratonajava.Isobrescrita.model.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+        System.out.println(anime);
+    }
+}
