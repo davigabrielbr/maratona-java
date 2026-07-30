3865a35360c115d3f7312752cfcb8094e8ab307e
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-07-30 16:30:17 -0300

    77 - Orientação Objetos - Modificador final pt 01 - Tipo primitivo

---
+++ src/academy/devdojo/maratonajava/Jmodificadorfinal/test/CarroTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.Jmodificadorfinal.test;
+
+import academy.devdojo.maratonajava.Jmodificadorfinal.model.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+    }
+}
