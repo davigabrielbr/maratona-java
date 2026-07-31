a482dc4b83c923a30edd78d493a64c0593efa39d
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-07-31 17:00:37 -0300

    82 - Orientação Objetos - Enumeração pt 03 - Sobrescrita de métodos

--- src/academy/devdojo/maratonajava/Kenum/model/Cliente.java
+++ src/academy/devdojo/maratonajava/Kenum/model/Cliente.java
@@ -1,10 +1,6 @@
 package academy.devdojo.maratonajava.Kenum.model;
 
 public class Cliente {
-    public enum TipoPagamento {
-        DEBITO, CREDITO
-    }
-
     private String nome;
     private TipoCliente tipoCliente;
     private TipoPagamento tipoPagamento;
