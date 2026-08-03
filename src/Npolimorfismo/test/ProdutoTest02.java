45100ba6236cf08ce6624ba3491cbc7c71668a13
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-08-03 18:02:25 -0300

    91 - Orientação Objetos - Polimorfismo pt 02 - Funcionamento

---
+++ src/Npolimorfismo/test/ProdutoTest02.java
@@ -0,0 +1,21 @@
+package Npolimorfismo.test;
+
+import Npolimorfismo.model.Computador;
+import Npolimorfismo.model.Produto;
+import Npolimorfismo.model.Tomate;
+
+public class ProdutoTest02 {
+    public static void main(String[] args) {
+        Produto produto = new Computador("Ryzen 9", 3000);
+        System.out.println(produto.getNome());
+        System.out.println(produto.getValor());
+        System.out.println(produto.calcularImposto());
+
+        System.out.println("--------------");
+
+        Produto produto2 = new Tomate("Americano", 20);
+        System.out.println(produto2.getNome());
+        System.out.println(produto2.getValor());
+        System.out.println(produto2.calcularImposto());
+    }
+}
