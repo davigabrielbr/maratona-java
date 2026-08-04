62c99998c54abdfd48045d405739f7d94231710e
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-08-04 17:35:26 -0300

    102 - Exceções pt 08 - Multi catch em linha

--- src/academy/devdojo/maratonajava/Oexception/runtime/test/RunTimeExceptionTest04.java
+++ src/academy/devdojo/maratonajava/Oexception/runtime/test/RunTimeExceptionTest04.java
@@ -8,28 +8,20 @@ public class RunTimeExceptionTest04 {
     public static void main(String[] args) {
         try {
             throw new RuntimeException();
-        } catch (ArrayIndexOutOfBoundsException e) {
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException e) {
             System.out.println("Dentro do ArrayIndexOutOfBoundsException");
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("Dentro do IndexOutOfBoundsException");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Dentro do IllegalArgumentException");
-        } catch (ArithmeticException e) {
-            System.out.println("Dentro do ArithmeticException");
         } catch (RuntimeException e) {
-            System.out.println("Dentro do RuntimeException");
+            System.out.println("Dentro do RunTimeException");
         }
 
         try {
             talvezLanceException();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
         }
     }
 
-    private static void talvezLanceException() throws SQLException, FileNotFoundException {
+    private static void talvezLanceException() throws SQLException, IOException {
 
     }
 }
