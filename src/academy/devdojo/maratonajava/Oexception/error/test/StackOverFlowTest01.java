86542cd7f371016bb47117a574380315b873d9d3
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-08-04 15:43:32 -0300

    96 - Exceções pt 02 - RuntimeException

--- src/academy/devdojo/maratonajava/Oexception/error/test/StackOverFlowTest01.java
+++ src/academy/devdojo/maratonajava/Oexception/error/test/StackOverFlowTest01.java
@@ -1,4 +1,4 @@
-package academy.devdojo.maratonajava.Oexception.test;
+package academy.devdojo.maratonajava.Oexception.error.test;
 
 public class StackOverFlowTest01 {
     public static void main(String[] args) {
---
+++ src/academy/devdojo/maratonajava/Oexception/runtime/test/RunTimeExceptionTest01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.Oexception.runtime.test;
+
+public class RunTimeExceptionTest01 {
+    public static void main(String[] args) {
+        // Checked e Uncheked
+        int[] nums = {1, 2};
+        System.out.println(nums[2]);
+    }
+}
