6b5efda032909bf6fd213fa98004a4898e775ba1
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-08-04 16:45:33 -0300

    98 - Exceções pt 04 - Lançando exceção unchecked

---
+++ src/academy/devdojo/maratonajava/Oexception/runtime/test/RunTimeExceptionTest02.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.Oexception.runtime.test;
+
+public class RunTimeExceptionTest02 {
+    public static void main(String[] args) {
+        divisao(1, 0);
+
+        System.out.println("Código finalizado");
+    }
+
+    private static int divisao(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Argumento ilegal, não pode ser 0");
+        }
+
+        return a / b;
+    }
+}
