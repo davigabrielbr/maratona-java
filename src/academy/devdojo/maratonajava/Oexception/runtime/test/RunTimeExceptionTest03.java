1b0996ae7b12dc56571e16941d02e7ccfc191c32
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-08-04 17:17:07 -0300

    100 - Exceções pt 06 - Bloco Finally

---
+++ src/academy/devdojo/maratonajava/Oexception/runtime/test/RunTimeExceptionTest03.java
@@ -0,0 +1,31 @@
+package academy.devdojo.maratonajava.Oexception.runtime.test;
+
+public class RunTimeExceptionTest03 {
+    public static void main(String[] args) {
+        abreConexao();
+    }
+
+    private static String abreConexao() {
+        try {
+            System.out.println("Abrindo arquivo");
+            System.out.println("Escrevendo dados no arquivo");
+            return "conexão aberta";
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Fechando recurso liberado pelo SO");
+        }
+
+        return null;
+    }
+
+    private static void abreConexao2() {
+        try {
+            System.out.println("Abrindo arquivo");
+            System.out.println("Escrevendo dados no arquivo");
+            throw new RuntimeException();
+        } finally {
+            System.out.println("Fechando recurso liberado pelo SO");
+        }
+    }
+}
