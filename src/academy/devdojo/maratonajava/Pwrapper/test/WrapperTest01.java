4883f6fcedbce2d63a3686696d1a585bb4f40b66
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-08-08 19:48:22 -0300

    106 - Classes Utilitárias - Wrappers pt 01

---
+++ src/academy/devdojo/maratonajava/Pwrapper/test/WrapperTest01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.Pwrapper.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 100;
+        char charP = 'W';
+        boolean booleanP = false;
+
+        Byte byteW = 127;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 100.0;
+        Character charW = 'W';
+        Boolean booleanW = false;
+    }
+}
