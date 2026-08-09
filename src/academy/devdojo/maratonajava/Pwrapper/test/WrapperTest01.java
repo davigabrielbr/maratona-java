75d9a2a63e1f22d717aa225984b02b4e331f1e03
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-08-09 19:32:47 -0300

    107 - Classes Utilitárias - Wrappers pt 02

--- src/academy/devdojo/maratonajava/Pwrapper/test/WrapperTest01.java
+++ src/academy/devdojo/maratonajava/Pwrapper/test/WrapperTest01.java
@@ -13,11 +13,23 @@ public class WrapperTest01 {
 
         Byte byteW = 127;
         Short shortW = 1;
-        Integer intW = 1;
+        Integer intW = 1; // autoboxing
         Long longW = 10L;
         Float floatW = 10F;
         Double doubleW = 100.0;
         Character charW = 'W';
         Boolean booleanW = false;
+
+        int i = intW; // umboxing
+        Integer intw2 = Integer.parseInt("1");
+        boolean verdadeiro = Boolean.parseBoolean("truE");
+        System.out.println(verdadeiro);
+
+        System.out.println(Character.isDigit('A'));
+        System.out.println(Character.isDigit('9'));
+        System.out.println(Character.isUpperCase('A'));
+        System.out.println(Character.isLetterOrDigit('!'));
+        System.out.println(Character.toUpperCase('a'));
+        System.out.println(Character.toLowerCase('A'));
     }
 }
