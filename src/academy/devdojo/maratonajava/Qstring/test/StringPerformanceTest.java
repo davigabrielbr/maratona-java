fdf0ad600a0f895b2ad33f73ddcf09fe169d78f9
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-08-12 16:02:46 -0300

    110 - Classes Utilitárias - Strings pt 03 - Desempenho

---
+++ src/academy/devdojo/maratonajava/Qstring/test/StringPerformanceTest.java
@@ -0,0 +1,44 @@
+package academy.devdojo.maratonajava.Qstring.test;
+
+public class StringPerformanceTest {
+    public static void main(String[] args) {
+        long inicio = System.currentTimeMillis();
+        concatString(30_000);
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para String: " + (fim - inicio) + "ms");
+
+        inicio = System.currentTimeMillis();
+        concatStringBuilder(10_000_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para StringBuilder: " + (fim - inicio) + "ms");
+
+        inicio = System.currentTimeMillis();
+        concatStringBuffer(10_000_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para StringBuilder: " + (fim - inicio) + "ms");
+    }
+
+    public static void concatString(int tamanho) {
+        String texto = "";
+
+        for (int i = 0; i < tamanho; i++) {
+            texto += i;
+        }
+    }
+
+    public static void concatStringBuilder(int tamanho) {
+        StringBuilder sb = new StringBuilder(tamanho);
+
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i);
+        }
+    }
+
+    public static void concatStringBuffer(int tamanho) {
+        StringBuffer sb = new StringBuffer(tamanho);
+
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i);
+        }
+    }
+}
